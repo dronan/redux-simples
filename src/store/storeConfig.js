28d1acf5bfa4b0857a49408ac71b8d8b1521f8da
Diego Ronan Kulian <dkulian@veepee.com>
2023-07-21 17:13:06 +0200

    feat(actioncreator): invocando as actions

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -2,13 +2,24 @@ import { legacy_createStore as createStore, combineReducers } from "redux";
 
 const reducers = combineReducers({
     numeros: function(state, action) {
-        console.log(state, " ", action);
-        return {
-            min: 0,
-            max: 2023
+        switch(action.type) {
+            case 'NUM_MIN_ALTERADO':
+                return {
+                    ...state,
+                    min: action.payload
+                }
+            case 'NUM_MAX_ALTERADO':
+                return {
+                    ...state,
+                    max: action.payload
+                }
+            default:
+                return {
+                    min: 0,
+                    max: 2023
+                }
         }
     }, nomes: function(state, action) {
-        console.log(state, " ", action);
         return [ 'Ana', 'Bia', 'Carlos']
     }
    
