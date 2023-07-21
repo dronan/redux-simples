237d7b56902fc7f1b9e2439cb78e0ffbd2db5450
Diego Ronan Kulian <dkulian@veepee.com>
2023-07-21 12:07:35 +0200

    feat(componentes): criando a estrutura inicial de componentes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import Card from './components/Card';
+import Intervalo from './components/Intervalo';
+import Media from './components/Media';
+import Soma from './components/Soma';
+import Sorteio from './components/Sorteio';
 
 function App() {
   return (
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
         <div className='linha'>
-          <Card title="Card 1" red>oi</Card>
-          <Card title="Card 2" green>oi</Card>
-          <Card title="Card 3" blue>oi</Card>
-          <Card title="Card 4" purple>oi</Card>
+          <Intervalo />
         </div>
         <div className='linha'>
-          <Card title="Card 2">
-            oi
-          </Card>
+          <Media />
+          <Soma />
+          <Sorteio />
         </div>
     </div>
   );
